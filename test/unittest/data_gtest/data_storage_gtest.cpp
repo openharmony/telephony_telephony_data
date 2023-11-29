b99c94b9df20ec59cd1e22e4e4ad4093c3820b50
liuxiyao223 <liuxiyao223@huawei.com>
2023-11-29 15:18:04 +0800

    protocol、apn_roam_protocol、is_roaming_apn字段从json配置文件读取 Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- test/unittest/data_gtest/data_storage_gtest.cpp
+++ test/unittest/data_gtest/data_storage_gtest.cpp
@@ -333,6 +333,9 @@ int DataStorageGtest::PdpProfileInsert(const std::shared_ptr<DataShare::DataShar
     value.Put(PdpProfileData::EDITED_STATUS, "1");
     value.Put(PdpProfileData::SERVER, "pdpserver");
     value.Put(PdpProfileData::BEARING_SYSTEM_TYPE, "1");
+    value.Put(PdpProfileData::IS_ROAMING_APN, "0");
+    value.Put(PdpProfileData::APN_PROTOCOL, "IPV4");
+    value.Put(PdpProfileData::APN_ROAM_PROTOCOL, "IPV6");
     return helper->Insert(uri, value);
 }
 
